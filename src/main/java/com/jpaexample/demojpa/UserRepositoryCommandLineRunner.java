ea93aa51b2d88f1deb9e35fa5100666b8b25fda5
Igor Kolodziejczyk (IKL) <igor.k.kolodziejczyk@gmail.com>
2019-01-22 18:01:41 +0100

    Usege of findById and findAll methods

--- src/main/java/com/jpaexample/demojpa/UserRepositoryCommandLineRunner.java
+++ src/main/java/com/jpaexample/demojpa/UserRepositoryCommandLineRunner.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 
@@ -17,5 +19,17 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User user = new User("Agnieszka", "Admin");
         System.out.println(userRepository.save(user).getId());
+
+        //Usage of findById
+        Optional<User> userOptional = userRepository.findById(1L);
+        if (userOptional.isPresent()) {
+            System.out.println("User from user repository " + userOptional.get().toString());
+        } else {
+            System.out.println("User wasn't found");
+        }
+
+        //Usage of findAll
+        System.out.println("List of all users");
+        userRepository.findAll().stream().forEach(userFromList->System.out.println(userFromList.toString()));
     }
 }
